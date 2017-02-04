e9fee0f0bea60bceaff10f74d4debf169b683386
Petteri Suominen <pspfolio@gmail.com>
2017-02-04 18:07:31 +0200

    basic UI

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import Nav from '../Nav/index.js';
+import Nav from '../Nav';
+import Table from '../Table';
+import './dashboard.css';
 
 export default class dlDashboard extends Component {
   render() {
     return (
       <section>
         <Nav />
-        <h1>Dashboard11</h1>
+        <section className='flex-container-site'>
+          <h2 className='site-header'>Deadlines</h2>
+        </section>
+        <Table />
       </section>
 
     )
--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './table.css';
 
-export default class dlTable extends React.Component({
+export default class Table extends Component {
   render() {
     return (
-      <h1>Table</h1>
+      <div className='flex-container-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Customer</th>
+              <th>Deadline</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Oma talous</td>
+              <td>Ovenia</td>
+              <td>28.02.2017</td>
+              <td>24 days to go</td>
+              <td>Ok</td>
+            </tr>
+            <tr>
+              <td>Oma talous</td>
+              <td>Ovenia</td>
+              <td>28.02.2017</td>
+              <td>24 days to go</td>
+              <td>Ok</td>
+            </tr>
+            <tr>
+              <td>Oma talous</td>
+              <td>Ovenia</td>
+              <td>28.02.2017</td>
+              <td>24 days to go</td>
+              <td>Ok</td>
+            </tr>
+            <tr>
+              <td>Oma talous</td>
+              <td>Ovenia</td>
+              <td>28.02.2017</td>
+              <td>24 days to go</td>
+              <td>Ok</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
-});
+}
