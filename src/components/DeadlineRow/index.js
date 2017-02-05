f0d7261732a87511505e594cc4c62f847904850e
Petteri Suominen <pspfolio@gmail.com>
2017-02-05 15:10:37 +0200

    moment.js to project use it in deadline list

--- src/components/DeadlineRow/index.js
+++ src/components/DeadlineRow/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 export default ({ deadline }) => (
   <tr>
     <td>{deadline.name}</td>
     <td>{deadline.customer}</td>
     <td>{deadline.date}</td>
-    <td>24 days to go</td>
+    <td>{moment(deadline.date, "DD.MM.YYYY").fromNow()}</td>
     <td>Ok</td>
   </tr>
 )
