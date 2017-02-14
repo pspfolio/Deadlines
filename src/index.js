00eafaf8146e8dd6d8113175b3f1dc63c46446ae
Petteri Suominen <pspfolio@gmail.com>
2017-02-14 20:35:19 +0200

    removing login page, because we are going to use single login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory  } from 'react-router';
-import Root from './root';
+import { browserHistory, Router, Route, IndexRoute  } from 'react-router';
+import App from './App';
+import Dashboard from './components/Dashboard';
 import './index.css';
 
-ReactDOM.render(<Root history={browserHistory} />, document.getElementById('root'));
+
+ReactDOM.render(
+ (
+  <Router history={ browserHistory }>
+    <Route path='/' component={ App }>
+      <IndexRoute component={ Dashboard } />
+    </Route>
+  </Router>
+  ),
+ document.getElementById('root')
+);
