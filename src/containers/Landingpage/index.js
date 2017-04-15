1aa5cd693fbef3e1c3b139b4c35b1a23583f5bbd
Petteri Suominen <pspfolio@gmail.com>
2017-04-15 12:26:13 +0300

    styling info part of landing page

--- src/containers/Landingpage/index.js
+++ src/containers/Landingpage/index.js
@@ -7,7 +7,7 @@ export default class Landingpage extends Component {
         return (
             <div className='content-wrapper'>
                 <header className='content-header'>
-                    <nav className='header-flex-container'>
+                    <nav className='landing-flex-container space-between'>
                         <div className='main-logo forward'>
                             DEADLINES
                         </div>
@@ -17,7 +17,7 @@ export default class Landingpage extends Component {
                         </ul>
                     </nav>
 
-                    <section id='header-text' className='header-flex-container'>
+                    <section id='header-text' className='landing-flex-container space-between'>
                         <h1 className='forward'>Are you ready to hit your deadlines?</h1>
                         <p className='forward'>Deadlines helps you and your team to track deadlines and makes prioritizing easier. Don't miss deadlines never again.</p>
                         <button className='button-started forward' type='button'>Get Started</button>
@@ -28,6 +28,15 @@ export default class Landingpage extends Component {
                         <span></span>
                     </div>
                 </header>
+
+                <div className='landing-flex-container space-around info'>
+                    <section className='info-section'>
+                        <h3>Track your deadlines</h3>
+                        <p>Tacos poke yr tbh sriracha. Bespoke kinfolk glossier chillwave migas fingerstache. Helvetica snackwave ugh mlkshk prism retro. Skateboard flannel art party lomo meditation blog.</p>
+                    </section>
+                    <img alt='icon of tracking deadlines' src='img/track.svg' />
+                </div>
+
             </div>
         )
     }
